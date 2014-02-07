096b816b08f3c3608035b9f9e52e7cb5d771faf1
thechewu <the_chewu@yahoo.com>
2014-02-07 09:51:22 -0500

    Added calculations and results

--- js/main.js
+++ js/main.js
@@ -1,16 +1,67 @@
+// Global vars
+var options;
+
 // Initial setup of the page
 $(document).ready(function(){
+	initVariables();
 	$("#selectOptions").html(createOptions());
+	$("#langSelect").val(100).change();
+	$("#effortSlider").slider({
+		min:20,
+		max:100,
+		value:100,
+		animate:"fast"
+	});
+	$("#complexitySlider").slider({
+		min:1,
+		max:10,
+		value:5,
+		animate:"fast"
+	});
+	// Calculate the results
+	$("#btnCalculate").click(function(){
+		var sloc = parseInt($("#inputFp").val())*parseInt($("#langSelect").val());
+		var effort = $( "#effortSlider" ).slider( "value" )/100;
+		var complexity = 1+(($( "#complexitySlider" ).slider( "value" )*0.024));
+		// Constant has gone up from 1998 and this reflects the change
+		var personMonth = 3 * effort * Math.pow((sloc/1000),complexity);
+		var cost = personMonth * parseInt($("#inCpp").val());
+		var coefficient = (3+(Math.log(sloc)/Math.LN10)/10)/10;
+		var schedule = 3.2 * Math.pow(personMonth,coefficient);
+		
+		
+		$("#personMonth").html(Math.round(personMonth*10)/10);
+		$("#totalCost").html("$"+Math.round(cost));
+		$("#schedule").html((Math.round(schedule*10)/10)+" Months ");
+		$("#slocEst").html(Math.round(sloc));
+		$("#results").show();
+	});
+	
 });
 
-// Calculate the results
-$("#btnCalculate").click(function(){
-	var sloc = parseInt($("#inputFp").val())*
-});
+
 
 // Values from: http://www.qsm.com/resources/function-point-languages-table
 function createOptions(){
-	var options = [
+	var optionStr="<select class='form-control' id='langSelect'>";
+	for(var i=0;i<options.length;i++){
+		optionStr+="<option value="+options[i].value+">"+options[i].name+"</option>";
+	}
+	optionStr+="</select>";
+	return optionStr;
+}
+
+function getOption(name){
+	for(var i=0;i<options.length;i++){
+		if(options[i].name === name){
+			return options[i].value;
+		}
+	}
+	return null;
+}
+
+function initVariables(){
+	options = [
 		{name:'ASP',value:51},
 		{name:'Assembler',value:119},
 		{name:'C',value:100},
@@ -27,12 +78,4 @@ function createOptions(){
 		{name:'VB .NET',value:52},
 		{name:'Visual Basic',value:42},
 	];
-	var optionStr="<select class='form-control'>";
-	
-	for(var i=0;i<options.length;i++){
-		optionStr+="<option value="+options[i].value+">"+options[i].name+"</option>";
-	}
-	
-	optionStr+="</select>";
-	return optionStr;
 }
